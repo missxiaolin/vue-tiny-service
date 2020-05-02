91e94dac44bbef5ced20d5b86a494b673d7e4bb5
xiaolin <462441355@qq.com>
2020-05-02 11:12:37 +0800

    * 跟新视图

--- main-container/src/main.js
+++ main-container/src/main.js
@@ -4,16 +4,22 @@ import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+// 导入乾坤函数
 import {
   registerMicroApps,
   runAfterFirstMounted,
   setDefaultMountApp,
   start
 } from "qiankun";
+// 按需导入element-ui
 import "./plugins/element.js";
+// 导入封装后的ui组件
+import "./library/ui/install";
+// 导入公共工具函数
+import { genActiveRule, routerGo } from "./library/js/util"
 
 let app = null;
-/* function render({ appContent, loading }) {
+function render({ appContent, loading } = {}) {
   if (!app) {
     app = new Vue({
       el: "#container",
@@ -38,39 +44,7 @@ let app = null;
     app.content = appContent;
     app.loading = loading;
   }
-}; 
-*/
-function render({ appContent, loading } = {}) {
-  if (!app && appContent !== undefined) {
-    app = new Vue({
-      el: "#container",
-      router,
-      store,
-      data() {
-        return {
-          content: appContent,
-          loading
-        };
-      },
-      render(h) {
-        return h(App, {
-          props: {
-            content: this.content,
-            loading: this.loading
-          }
-        });
-      }
-    });
-  } else if (app == null && appContent !== undefined) {
-    app.content = appContent;
-    app.loading = loading;
-  }
-}
-
-function genActiveRule(routerPrefix) {
-  return location => location.pathname.startsWith(routerPrefix);
-}
-
+};
 render();
 
 // 定义传入子应用的数据
@@ -80,10 +54,8 @@ let msg = {
   },
   fns: [
     {
-      name: "LOGOUT_",
-      LOGOUT_(data) {
-        alert('父应用返回信息：' + data)
-      }
+      name: "routerGo",
+      routerGo
     }
   ]
 };
@@ -93,20 +65,20 @@ registerMicroApps(
   [
     {
       name: "module-basic-data",
-      entry: "//localhost:7771",
+      entry: "//localhost:6651",
       render,
       activeRule: genActiveRule("/basic"),
       props: msg
     },
     {
       name: "module-report",
-      entry: "//localhost:7772",
+      entry: "//localhost:6652",
       render,
       activeRule: genActiveRule("/report"),
       props: msg
     }
-  ]
-  /* {
+  ],
+  {
     beforeLoad: [
       app => {
         console.log("before load", app);
@@ -122,7 +94,7 @@ registerMicroApps(
         console.log("after unload", app);
       }
     ]
-  } */
+  }
 );
 
 // 设置默认子应用
@@ -130,7 +102,7 @@ setDefaultMountApp("/basic");
 // 第一个子应用加载完毕回调
 runAfterFirstMounted();
 // 启动微服务
-start();
+start({ prefetch: true });
 
 /* new Vue({
   router,
